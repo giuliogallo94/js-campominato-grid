fc1a9c0db81aa01200433dc5acc98cacb27de0c1
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-28 09:21:21 +0200

    add: js grid generate function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // ESECUZIONE
 
+button.addEventListener("click", generateGridCell)
+
+for (let i = 1; i <= 100; i++) {
+    const gridNumber = i
+    //Genera la cella
+    const cell = generateGridCell(gridNumber)
+}
 
 /************/
 // FUNZIONI
+/**
+ * Genera una cella della griglia con un numero all'interno
+ * @param {number} innerNumber
+ * @returns {any} elemento HTML che rappresenta la cella
+ */
+function generateGridCell(innerNumber) {
+    const newCell = document.createElement("div");
+    newCell.classList.add("cell");
+    newCell.innerHTML = innerNumber;
+    return newCell;
+  }
+  
