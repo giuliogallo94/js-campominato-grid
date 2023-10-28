c361d0f6722f9293b9904eaa85fc5b83cae8e808
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-28 11:56:27 +0200

    refactoring: function add on JS

--- js/script.js
+++ js/script.js
@@ -7,40 +7,13 @@ console.log(gameLevel);
 
 if (gameLevel === "1") {
   console.log("ciao");
-  for (let i = 1; i <= 100; i++) {
-    let gridNumber = i;
-    console.log(gridNumber);
-    let cell = generateGridCell(gridNumber);
-    cell.addEventListener("click", function () {
-      cell.classList.add("bg-cyan");
-    });
-    gridElem.append(cell);
-    cell.classList.add("cell-w-1");
-  }
+  gridCreation(100, "cell-w-1");
 } else if (gameLevel === "2") {
   console.log("ciao");
-  for (let i = 1; i <= 81; i++) {
-    let gridNumber = i;
-    console.log(gridNumber);
-    let cell = generateGridCell(gridNumber);
-    cell.addEventListener("click", function () {
-      cell.classList.add("bg-cyan");
-    });
-    gridElem.append(cell);
-    cell.classList.add("cell-w-2");
-  }
+  gridCreation(81, "cell-w-2");
 } else if (gameLevel === "3") {
   console.log("ciao");
-  for (let i = 1; i <= 49; i++) {
-    let gridNumber = i;
-    console.log(gridNumber);
-    let cell = generateGridCell(gridNumber);
-    cell.addEventListener("click", function () {
-      cell.classList.add("bg-cyan");
-    });
-    gridElem.append(cell);
-    cell.classList.add("cell-w-3");
-  }
+  gridCreation(49, "cell-w-3");
 }
 
 const playBtnElem = document.getElementById("play");
@@ -48,8 +21,9 @@ playBtnElem.addEventListener("click", showGrid);
 
 /************/
 // FUNZIONI
+
 /**
- * Genera una cella della griglia con un numero all'interno
+ * Creazione cella
  * @param {number} innerNumber
  * @returns {any} elemento HTML che rappresenta la cella
  */
@@ -58,6 +32,25 @@ function generateGridCell(innerNumber) {
   newCell.classList.add("cell");
   newCell.innerHTML = innerNumber;
   return newCell;
+}
+
+/**
+ * Creazione griglia
+ * @param {any}
+ * @param {any}
+ * viene generato il giusto numero di celle, con la giusta disposizione in base alla difficoltà del gioco
+ */
+function gridCreation(cellNum, cellW) {
+  for (let i = 1; i <= cellNum; i++) {
+    let gridNumber = i;
+    console.log(gridNumber);
+    let cell = generateGridCell(gridNumber);
+    cell.addEventListener("click", function () {
+      cell.classList.add("bg-cyan");
+    });
+    gridElem.append(cell);
+    cell.classList.add(cellW);
+  }
 }
 
 /**
@@ -74,8 +67,8 @@ function showGrid() {
  * Colora la cella cliccata
  * @param {any}
  * @returns {any} aggiunta classe "bg-cyan"
- */
-function alternativeBg() {
-  const clickedCell = clickedCell.classList.add("bg-cyan");
-  console.log($gridNumber);
-}
+//  */
+// function alternativeBg() {
+//   const clickedCell = clickedCell.classList.add("bg-cyan");
+//   console.log($gridNumber);
+// }
